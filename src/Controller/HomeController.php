30b5c3c97f8ffb22db767e83a542509efef5608c
laheravilla <laheravilla@gmail.com>
2019-06-05 17:01:19 +0200

    Deleting inactive use

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Picture;
 use App\Repository\PictureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;

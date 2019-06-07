25d712c226b8b996c59f694a04a75ab01dfdbb61
MaistrePedro <peterpendrag@gmail.com>
2019-06-07 10:10:32 +0200

    findBy ajouté

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,13 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-    public function index(): Response
+    public function index(EventRepository $eventRepository): Response
     {
+        $first = $eventRepository->findBy([],['date'=>'DESC'],1);
+//        dd($first);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'first' => $first,
         ]);
     }
 }

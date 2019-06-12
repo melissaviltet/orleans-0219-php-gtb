ae55a55a856d4d22cfbef8540570dad8b9bf065c
Pedro <peterpendrag@gmail.com>
2019-06-12 14:53:44 +0200

    html et appel des entités

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AssociationRepository;
 use App\Repository\SponsorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @param SponsorRepository $sponsorRepository
+     * @param SponsorRepository $sponsorRepository AssociationRepository $association
      * @Route("/", name="home")
      * @return Response
      */
-    public function index(SponsorRepository $sponsorRepository): Response
+    public function index(SponsorRepository $sponsorRepository, AssociationRepository $association): Response
     {
         return $this->render('home/index.html.twig', [
             'sponsors' => $sponsorRepository->findAll(),
+            'association' => $association->findAll(),
         ]);
     }
 }

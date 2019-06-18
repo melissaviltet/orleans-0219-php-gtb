fbe6c9477d330212ec83c3c746a8469b8ed1da0e
Pedro <peterpendrag@gmail.com>
2019-06-18 15:17:26 +0200

    created 1 fixture file for each entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -22,27 +22,6 @@ class AppFixtures extends Fixture
             $sponsor->setSite($faker->url);
             $manager->persist($sponsor);
         }
-        $association = new Association();
-        $association->setTrailContent($faker->text);
-        $association->setTrailHome($faker->text);
-        $association->setTriathlonContent($faker->text);
-        $association->setTriathlonHome($faker->text);
-        $association->setClubContent($faker->text);
-        $association->setClubHome($faker->text);
-        $manager->persist($association);
-
-        for ($i = 0; $i < 4; $i++) {
-            $event = new Event();
-            $event->setName($faker->sentence(3));
-            $event->setUrl($faker->url);
-            $event->setDate($faker->dateTime);
-            $event->setPlace($faker->city);
-            $event->setIsPrivate($faker->boolean);
-            $event->setPicture($faker->imageUrl());
-            $manager->persist($event);
-        }
-
-
         $manager->flush();
     }
 }

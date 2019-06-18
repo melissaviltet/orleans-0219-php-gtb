179df3bbea928ee84a2bee4f4a33d8bbcb4bb297
laheravilla <laheravilla@gmail.com>
2019-06-18 13:54:30 +0200

    jQuery Navbar colors change according to scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,28 @@ $(document).ready(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('nav.navbar').css({
-                "background-color": "rgba(97, 97, 97, .9)"
+                "background-color": "rgba(97, 97, 97, .9)",
+                WebkitTransition : 'all .5s ease-in-out',
+                MozTransition    : 'all .5s ease-in-out',
+                MsTransition     : 'all .5s ease-in-out',
+                OTransition      : 'all .5s ease-in-out',
+                transition       : 'all .5s ease-in-out'
             });
             $('.navbar-nav .nav-link').css({
-                "color": "rgb(230, 231, 233)"
+                color            : 'rgb(230, 231, 233)',
             });
         } else {
             $('nav.navbar').css({
-                "background-color": "rgba(97, 97, 97, .3)"
+                "background-color": "rgba(97, 97, 97, .3)",
+                WebkitTransition : 'all .5s ease-in-out',
+                MozTransition    : 'all .5s ease-in-out',
+                MsTransition     : 'all .5s ease-in-out',
+                OTransition      : 'all .5s ease-in-out',
+                transition       : 'all .5s ease-in-out'
             });
             $('.navbar-nav .nav-link').css({
-                "color": "white"});
+                color            : "white",
+            });
         }
     });
 });

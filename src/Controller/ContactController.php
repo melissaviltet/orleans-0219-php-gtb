5ed8dafdec2b09406a6146b72e35853e5a8c8c95
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2019-06-24 17:46:26 +0200

    ajout du titre de section

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class ContactController extends AbstractController
 {
     /**

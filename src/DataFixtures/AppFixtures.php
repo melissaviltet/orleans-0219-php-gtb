bad22deffddfa2abb40d4790abfd771cdd729534
laheravilla <laheravilla@gmail.com>
2019-07-02 11:27:05 +0200

    Show one event to come by Id

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -18,7 +18,6 @@ class AppFixtures extends Fixture
             $sponsor->setName($faker->sentence(1));
             $sponsor->setImageName('logo_veloland.jpg');
             $sponsor->setImageSize(1024);
-            $sponsor->setUpdatedAt($faker->dateTime());
             $sponsor->setSite($faker->url);
             $manager->persist($sponsor);
         }

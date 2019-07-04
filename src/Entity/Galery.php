72e2316ef8c8030d3ea084315548c14b9b752bfb
laheravilla <laheravilla@gmail.com>
2019-07-04 13:29:15 +0200

    Adding a image to the gallery from admin

--- src/Entity/Galery.php
+++ src/Entity/Galery.php
@@ -3,12 +3,24 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\GaleryRepository")
+ * @Vich\Uploadable
+ * @UniqueEntity(
+ *     fields={"alt"},
+ *     message="Ce image existe déjà"
+ * )
  */
 class Galery
 {
+    const AUTHORIZED_EXTENSIONS = ["image/jpg", "image/png", "image/jpeg"];
+    const MAX_SIZE = '1024k';
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -17,19 +29,58 @@ class Galery
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank(message="Champs requis")
+     * @Assert\Length(
+     *     min = 2,
+     *     max = 255,
+     *     minMessage = "Doit avoir au moins {{ limit }} caractères",
+     *     maxMessage = "Ne doit pas depasser les {{ limit }} caractères"
+     * )
      */
     private $alt;
+
+    /**
+     * @Vich\UploadableField(mapping="image_gallery", fileNameProperty="imageName", size="imageSize")
+     * @Assert\NotBlank(message="Champs requis")
+     * @Assert\File(
+     *     maxSize=Galery::MAX_SIZE,
+     *     maxSizeMessage="Image trop lourde!",
+     *     mimeTypes=Galery::AUTHORIZED_EXTENSIONS,
+     *     mimeTypesMessage="Extension de fichier non autorisée",
+     * )
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     * @Assert\Length(
+     *     min = 2,
+     *     max = 255,
+     *     minMessage = "Doit avoir au moins {{ limit }} caractères",
+     *     maxMessage = "Ne doit pas depasser les {{ limit }} caractères"
+     * )
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @var integer|null
+     */
+    private $imageSize;
+
+    /**
+     * @ORM\Column(type="date_immutable", nullable=true)
+     * @var \DateTimeImmutable|null
+     */
+    private $updatedAt;
 
     /**
      * @ORM\Column(type="boolean")
      */
     private $private;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $url;
 
     public function getId(): ?int
     {
@@ -60,15 +111,66 @@ class Galery
         return $this;
     }
 
-    public function getUrl(): ?string
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
     {
-        return $this->url;
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            $this->setUpdatedAt(new \DateTimeImmutable());
+        }
     }
 
-    public function setUrl(string $url): self
+    public function getImageFile(): ?File
     {
-        $this->url = $url;
+        return $this->imageFile;
+    }
 
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
+    }
+
+    /**
+     * @param int|null $imageSize
+     * @return Galery
+     */
+    public function setImageSize(?int $imageSize): Galery
+    {
+        $this->imageSize = $imageSize;
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable|null $updatedAt
+     * @return Galery
+     */
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): Galery
+    {
+        $this->updatedAt = $updatedAt;
         return $this;
     }
 }
---
+++ src/Form/GalleryType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Galery;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class GalleryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('alt', TextType::class, [
+                'label' => 'Nom de l\'image:'
+            ])
+            ->add('private', RadioType::class, [
+                'help' => '*Par default, l\'image est publique',
+                'label' => 'Image privée',
+            ])
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => true,
+                'help' => '*Poids maximum autorisé: ' . Galery::MAX_SIZE,
+                'label' => 'Image: ',
+                'attr' => [
+                    'placeholder' => 'Chosissez votre image: '
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Galery::class,
+        ]);
+    }
+}

d446da1bba34bf5eb692725e66a2ba7456a0882a
Pedro <peterpendrag@gmail.com>
2019-07-04 17:27:07 +0200

    starting us

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AssociationRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +12,15 @@ class ClubController extends AbstractController
 {
     /**
      * @param AssociationRepository $associationRepository
+     * @param UserRepository $userRepository
      * @Route("/club", name="club")
      * @return Response
      */
-    public function show(AssociationRepository $associationRepository): Response
+    public function show(AssociationRepository $associationRepository, UserRepository $userRepository): Response
     {
         return $this->render('club/index.html.twig', [
             'association' => $associationRepository->findOneBy([]),
+            'members' => $userRepository->findBy([], [''])
         ]);
     }
 }

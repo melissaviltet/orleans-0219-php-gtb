06d55261c6da4f11129e38810a1d17c450d47aa7
Pedro <peterpendrag@gmail.com>
2019-07-04 10:15:34 +0200

    security en cours

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -138,7 +138,7 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[0] = 'ROLE_USER';
 
         return array_unique($roles);
     }

31a18854673d3c5124e233530956bcfe36357b3f
Pedro <peterpendrag@gmail.com>
2019-07-08 13:52:43 +0200

    drying the code

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -16,25 +16,10 @@ class ClubController extends AbstractController
      * @Route("/club", name="club")
      * @return Response
      */
-    public function show(
-        AssociationRepository $associationRepository,
-        MembersGetting $membersGetting
-    ): Response {
-        $members = $membersGetting->sortMembers();
-        if (!isset($members['president'])) {
-            $members['president'] = [];
-        }
-        if (!isset($members['office'])) {
-            $members['office'] = [];
-        }
-        if (!isset($members['member'])) {
-            $members['member'] = [];
-        }
+    public function show(AssociationRepository $associationRepository): Response
+    {
         return $this->render('club/index.html.twig', [
             'association' => $associationRepository->findOneBy([]),
-            'members' => $members['member'],
-            'officeMembers' => $members['office'],
-            'presidents' => $members['president'],
         ]);
     }
 }

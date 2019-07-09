ecfc41304a0fc8ce66582658f9d193bfb20b86a8
laheravilla <laheravilla@gmail.com>
2019-07-09 14:35:34 +0200

    solved chages requested

--- src/Migrations/Version20190709084835.php
+++ src/Migrations/Version20190709084835.php
@@ -19,13 +19,6 @@ final class Version20190709084835 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('INSERT INTO user
-        (gender_id, email, roles, password, firstname, lastname, address, birthdate, telephone)
-        VALUES (1, \'admin@gmail.com\',
-            \'["ROLE_ADMIN"]\',
-            \'$argon2i$v=19$m=1024,t=2,p=2$d3V4Ukl6VHlIcXJxM0JFNQ$8Jm6/Z96Ebh/G2+f2el4RfxSVdpxYyq5W+N2BZSdbDc\',
-            \'admin\', \'trail\', \'1 rue du trail\', \'1990-01-01\', 0000000000);');
-
         $this->addSql('INSERT INTO activity (name) VALUES (\'Trail\'),(\'Triathlon\'),(\'Préparation physique\'),
         (\'natation\'),(\'cyclisme\'),(\'natation\');');
     }
